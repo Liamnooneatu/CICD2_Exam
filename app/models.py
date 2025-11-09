7c05fd57f4b719f4b6da00714b10491ec9bcc69e
plennon1 <55662945+plennon2019@users.noreply.github.com>
2025-11-09 10:06:55 +0000

    Add UniqueConstraint import to models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
 
 class Base(DeclarativeBase):
     pass
